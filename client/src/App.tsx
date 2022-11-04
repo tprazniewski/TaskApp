5eb6db9895d5713dd1fb339a50cb2146bd39f19b
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-04 10:21:18 +0100

    create custom theme based on the MUI component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import React, { FC, ReactElement } from "react";
-
+import { customTheme } from "./theme/customTheme";
+import { ThemeProvider, CssBaseline } from "@mui/material"; // CssBaseline resets the value based on the provided customTheme
 const App: FC = (): ReactElement => {
-  return <h1> Main Route</h1>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1> Main Route</h1>
+    </ThemeProvider>
+  );
 };
 export default App;
