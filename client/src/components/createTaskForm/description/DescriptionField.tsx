45395ba560f7863dccd091ef430d87d0bcfcba02
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-11 19:48:04 +0100

    update

--- client/src/components/createTaskForm/description/DescriptionField.tsx
+++ client/src/components/createTaskForm/description/DescriptionField.tsx
@@ -1,7 +1,9 @@
 import React, { FC, ReactElement } from "react";
 import { TextField } from "@mui/material";
+import { ITextField } from "../interfaces/ITextField";
 
-export const DescriptionField: FC = (): ReactElement => {
+export const DescriptionField: FC<ITextField> = (props): ReactElement => {
+  const { onChange = (e) => console.log(e), disabled = false } = props;
   return (
     <TextField
       id="description"
@@ -12,6 +14,8 @@ export const DescriptionField: FC = (): ReactElement => {
       multiline
       rows={4}
       fullWidth
+      disabled={disabled}
+      onChange={onChange}
     />
   );
 };
