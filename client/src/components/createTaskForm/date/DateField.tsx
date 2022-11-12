b7769bbe77def9e52ee08f1e08d21b759a3dd7dc
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-12 13:37:45 +0100

    Dreate IDate interface and proptypes in DateField

--- client/src/components/createTaskForm/date/DateField.tsx
+++ client/src/components/createTaskForm/date/DateField.tsx
@@ -1,22 +1,35 @@
-import React, { FC, ReactElement, useState } from "react";
+import React, { FC, ReactElement } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DesktopDatePicker } from "@mui/x-date-pickers";
 import { TextField } from "@mui/material";
+import { IDate } from "../interfaces/IDate";
+import PropTypes from "prop-types";
 
-export const DataField: FC = (): ReactElement => {
-  const [date, setDate] = useState<Date | null>(null);
+export const DataField: FC<IDate> = (props): ReactElement => {
+  const {
+    disabled = false,
+    value = new Date(),
+    onChange = (date) => console.log(date),
+  } = props;
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <DesktopDatePicker
           label="Task Date"
           inputFormat="dd/MM/yyyy"
-          value={date}
-          onChange={(val) => setDate(val)}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
           renderInput={(params) => <TextField {...params} />}
         />
       </LocalizationProvider>
     </>
   );
+};
+
+DataField.propTypes = {
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  value: PropTypes.instanceOf(Date),
 };
